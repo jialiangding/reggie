c00fc985a8eba39a1e9a49a925bd0681ed7d8b83
dingjl <dingjl@gratone.cn>
2022-08-06 14:08:56 +0800

    请求过滤 防止未登录 也可以访问其他资源

--- reggie_backend/src/main/java/jialiang_ding/reggie/config/WebMvcConfig.java
+++ reggie_backend/src/main/java/jialiang_ding/reggie/config/WebMvcConfig.java
@@ -2,12 +2,16 @@ package jialiang_ding.reggie.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
 @Slf4j
 public class WebMvcConfig  extends WebMvcConfigurationSupport {
+    //路径比较 工具类 类似
+
+
     /**
      * 设置静态资源映射
      不做这个的话  默认是访问static 或者templeate下的静态资源文件
@@ -19,6 +23,10 @@ public class WebMvcConfig  extends WebMvcConfigurationSupport {
         //classpath 对应的就是recouse这个目录  当访问链接中带 /backend/ 的路径名称时 就访问到 classpath:/backend/
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+
+
+
+
 
     }
 }
--- reggie_backend/src/main/java/jialiang_ding/reggie/filter/LoginCheckFilter.java
+++ reggie_backend/src/main/java/jialiang_ding/reggie/filter/LoginCheckFilter.java
@@ -1,12 +1,15 @@
 package jialiang_ding.reggie.filter;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+
 
 /**
  * 检查用户是否完成登录
@@ -15,25 +18,49 @@ import java.io.IOException;
 @WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*")
 public class LoginCheckFilter  implements Filter {
 
-
+    public static  final AntPathMatcher antPathMatcher=new AntPathMatcher();
 
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request=(HttpServletRequest)servletRequest;
         HttpServletResponse response=(HttpServletResponse) servletResponse;
-        log.info("拦截到请求:{}",request.getRequestURL());
+
         //判断本次请求 是否需要处理
         String[] urls=new String[]{
                 "/employee/login",
                 "/employee/logout",
-                "/banckend/**",
-                "front/**"
+                "/backend/**",
+                "/front/**"
         };
 
+        if ( this.check(urls,request.getRequestURI())){
+            log.info("放行了");
+            filterChain.doFilter(request,response);
+        }
+        //判断访问的链接是否是需要先登录？ 如果是需要登录的  但是没有登录 拦截
+        Object employee = ((HttpServletRequest) servletRequest).getSession().getAttribute("employee");
+        if (employee!=null){
+            //非空说明已经登录过了
+            filterChain.doFilter(request,response);
+        }
 
 
-        //放行
-        filterChain.doFilter(request,response);
+
     }
+
+
+
+    public Boolean check(String[] urls,String requestUrl){
+//      Arrays.asList(urls).forEach(url-> antPathMatcher.match(url,requestUrl));
+        for ( String url:urls) {
+            log.info(url);
+            log.info(requestUrl);
+                boolean match = antPathMatcher.match(url, requestUrl);
+
+            return match;
+        }
+        return  false;
+        }
+
 }
